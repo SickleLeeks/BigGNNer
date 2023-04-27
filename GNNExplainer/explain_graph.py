c2f3483a675b75f5e88272c425aed0aeb98c0f69
SickleLeeks <pxz6603@126.com>
2023-04-27 14:53:07 +0800

    add two example about GNNexplainer

---
+++ GNNExplainer/explain_graph.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# Created by BIT09 at 2023/4/27
+import matplotlib.pyplot as plt
+import torch
+import torch.nn.functional as F
+from torch.nn import Linear
+from torch_geometric.datasets import TUDataset
+from torch_geometric.loader import DataLoader
+from torch_geometric.nn import GNNExplainer
+from torch_geometric.nn import GraphConv
+from torch_geometric.nn import global_mean_pool
+
+
+# Build the model
+class GNN(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super(GNN, self).__init__()
+        torch.manual_seed(12345)
+        self.conv1 = GraphConv(dataset.num_node_features, hidden_channels)
+        self.conv2 = GraphConv(hidden_channels, hidden_channels)
+
+        self.conv3 = GraphConv(hidden_channels, hidden_channels)
+        self.linear = Linear(hidden_channels, dataset.num_classes)
+
+    def forward(self, x, edge_index, batch):
+        x = self.conv1(x, edge_index)
+        x = x.relu()
+        x = self.conv2(x, edge_index)
+        x = x.relu()
+        x = self.conv3(x, edge_index)
+        x = global_mean_pool(x, batch)
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.linear(x)
+        return x
+
+
+# Creating the function to train the model
+def Train(data_loader, loss_func):
+    model.train()
+    # Iterate in batches over the training dataset
+    for data in data_loader:
+        # Perform a single forward pass
+        out = model(data.x, data.edge_index, data.batch)
+        # Compute the loss
+        loss = loss_func(out, data.y)
+        # Derive gradients
+        loss.backward()
+        # Update parameters based on gradients
+        optimizer.step()
+        # Clear gradients
+        optimizer.zero_grad()
+
+
+# function to test the model
+def Test(data_loader):
+    model.eval()
+    correct = 0
+    # Iterate in batches over the training/test dataset
+    for data in data_loader:
+        out = model(data.x, data.edge_index, data.batch)
+        # Use the class with highest probability.
+        pred = out.argmax(dim=1)
+        # Check against ground-truth labels.
+        correct += int((pred == data.y).sum())
+    # Derive ratio of correct predictions.
+    return correct / len(test_loader.dataset)
+
+
+if __name__ == '__main__':
+    # Load the dataset
+    dataset = TUDataset(root='../TUDataset', name='MUTAG')
+    # print details about the graph
+    print(f'Dataset: {dataset}:')
+    print("Number of Graphs: ", len(dataset))
+    print("Number of Freatures: ", dataset.num_features)
+    print("Number of Classes: ", dataset.num_classes)
+
+    data = dataset[0]
+    print(data)
+    print("No. of nodes: ", data.num_nodes)
+    print("No. of Edges: ", data.num_edges)
+    print(f'Average node degree: {data.num_edges / data.num_nodes:.2f}')
+    print(f'Has isolated nodes: {data.has_isolated_nodes()}')
+    print(f'Has self-loops: {data.has_self_loops()}')
+    print(f'Is undirected: {data.is_undirected()}')
+
+    # Create train and test dataset
+    torch.manual_seed(12345)
+    dataset = dataset.shuffle()
+
+    train_dataset = dataset[:50]
+    test_dataset = dataset[50:]
+    print(f'Number of training graphs: {len(train_dataset)}')
+    print(f'Number of test graphs:{len(test_dataset)}')
+
+    '''graphs in graph classification datasets are usually small,
+    a good idea is to batch the graphs before inputting
+    them into a Graph Neural Network to guarantee full GPU utilization__
+    _In pytorch Geometric adjacency matrices are stacked in a diagonal fashion
+    (creating a giant graph that holds multiple isolated subgraphs), a
+    nd node and target features are simply concatenated in the node dimension:
+    '''
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+    for step, data in enumerate(train_loader):
+        print(f'Step {step + 1}')
+        print('==============')
+        print(f'Number of graphs in the current batch: {data.num_graphs}')
+        print(data)
+        print()
+
+    # Build the model
+    model = GNN(hidden_channels=64)
+    print(model)
+
+    # set the optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.02)
+
+    # set the loss function
+    criterion = torch.nn.CrossEntropyLoss()
+
+    # Train the model for 150 epochs
+    for epoch in range(1, 100):
+        Train(train_loader, loss_func=criterion)
+        train_acc = Test(train_loader)
+        test_acc = Test(test_loader)
+        if epoch % 10 == 0:
+            '''print(f'Epoch {epoch:>3} | Train Loss: {total_loss/len(train_loader):.3f} '
+                  f'| Train Acc: {acc/len(train_loader)*100:>6.2f}% | Val Loss: '
+                  f'{val_loss/len(train_loader):.2f} | Val Acc: '
+                  f'{val_acc/len(train_loader)*100:.2f}%')
+            '''
+            print(f'Epoch: {epoch:03d}, Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}')
+
+    # Explain the graph
+    explainer = GNNExplainer(model, epochs=100, return_type='log_prob')
+    data = dataset[0]
+    node_feat_mask, edge_mask = explainer.explain_graph(data.x, data.edge_index)
+    ax, G = explainer.visualize_subgraph(-1, data.edge_index, edge_mask, data.y)
+    plt.show()
---
+++ GNNExplainer/explain_node.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# Created by BIT09 at 2023/4/26
+import matplotlib.pyplot as plt
+import torch
+import torch.nn.functional as F
+from torch_geometric.datasets import Planetoid
+from torch_geometric.loader import NeighborLoader
+from torch_geometric.nn import GCNConv, GNNExplainer
+
+
+# Define the GCN model
+class Net(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = GCNConv(dataset.num_features, 16, normalize=False)
+        self.conv2 = GCNConv(16, dataset.num_classes, normalize=False)
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.02, weight_decay=5e-4)
+
+    def forward(self, x, edge_index):
+        x = F.relu(self.conv1(x, edge_index))
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
+
+
+def accuracy(pred_y, y):
+    """Calculate accuracy"""
+    return ((pred_y == y).sum() / len(y)).item()
+
+
+# define the function to Train the model
+def train_nn(model, x, edge_index, epochs, device):
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = model.optimizer
+
+    model.train()
+    for epoch in range(epochs + 1):
+        total_loss = 0
+        acc = 0
+        val_loss = 0
+        val_acc = 0
+
+        # Train on batches
+        for batch in train_loader:
+            optimizer.zero_grad()
+            batch = batch.to(device)
+            out = model(batch.x, batch.edge_index)
+            loss = criterion(out[batch.train_mask], batch.y[batch.train_mask])
+
+            total_loss += loss
+            acc += accuracy(out[batch.train_mask].argmax(dim=1),
+                            batch.y[batch.train_mask])
+            loss.backward()
+            optimizer.step()
+
+            # Validation
+            val_loss += criterion(out[batch.val_mask], batch.y[batch.val_mask])
+            val_acc += accuracy(out[batch.val_mask].argmax(dim=1), batch.y[batch.val_mask])
+
+        # Print metrics every 10 epochs
+        if epoch % 10 == 0:
+            print(f'Epoch {epoch:>3} | Train Loss: {total_loss / len(train_loader):.3f} '
+                  f'| Train Acc: {acc / len(train_loader) * 100:>6.2f}% | Val Loss: '
+                  f'{val_loss / len(train_loader):.2f} | Val Acc: '
+                  f'{val_acc / len(train_loader) * 100:.2f}%')
+
+
+# define the function to Test the model
+def Test(model, data, device):
+    """Evaluate the model on test set and print the accuracy score."""
+    model.eval()
+    data = data.to(device)
+    out = model(data.x, data.edge_index)
+    acc = accuracy(out.argmax(dim=1)[data.test_mask], data.y[data.test_mask])
+    return acc
+
+
+if __name__ == '__main__':
+    # Load the Planetoid dataset
+    dataset = Planetoid(root='../', name='Pubmed')
+    data = dataset[0]
+    # Set the device dynamically
+    print(torch.cuda.is_available())
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    # Create batches with neighbor sampling
+    train_loader = NeighborLoader(data, num_neighbors=[5, 10], batch_size=16, input_nodes=data.train_mask)
+    model = Net().to(device)
+
+    # Train the model
+    train_nn(model, x=data.x, edge_index=data.edge_index, epochs=200, device=device)
+
+    # Test
+    acc = Test(model, data, device=device)
+    print(f'\nGCN test accuracy: {acc * 100:.2f}%\n')
+
+    # Explain the GCN for node
+    node_idx = 20
+    x, edge_index = data.x, data.edge_index
+    # Pass the model to explain to GNNExplainer
+    explainer = GNNExplainer(model, epochs=100, return_type='log_prob')
+    # returns a node feature mask and an edge mask that play a crucial role to explain the prediction made by the GNN for node 20
+    node_feat_mask, edge_mask = explainer.explain_node(node_idx, x, edge_index)
+    ax, G = explainer.visualize_subgraph(node_idx, edge_index, edge_mask, y=data.y)
+    plt.show()
+    print("Ground Truth label for node: ", node_idx, " is ", data.y.cpu().numpy()[node_idx])
+    out = torch.softmax(model(data.x, data.edge_index), dim=1).argmax(dim=1)
+    print("Prediction for node ", node_idx, "is ", out[node_idx].cpu().detach().numpy().squeeze())
